ee88cfae7881fca434f9f25286cd7319d2296870
ShadenKhaled85 <shaden_khaled85@yahoo.com>
2025-07-22 11:03:33 +0300

    Resolving tailwind issue

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { ICategory } from '../../shared/interfaces/icategory';
 import { IProduct } from '../../shared/interfaces/iproduct';
 import { ProductsService } from './../../core/services/product/products.service';
 import { Component, inject, OnInit } from '@angular/core';
+import { CarouselModule,  OwlOptions  } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CarouselModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -19,6 +20,31 @@ export class HomeComponent implements OnInit{
 
   private readonly productsService = inject(ProductsService)
   private readonly categoriesService = inject(CategoriesService)
+
+    customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 
   callProducts(){
     this.productsService.getProducts().subscribe({
