4dbd39313f814814375cc2f2f953f127920500ce
ShadenKhaled85 <shaden_khaled85@yahoo.com>
2025-07-27 15:31:52 +0300

    Commit

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
+import { Component, inject } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
 import { AlertComponent } from "../../shared/components/businesss/alert/alert.component";
+import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,15 +11,43 @@ import { AlertComponent } from "../../shared/components/businesss/alert/alert.co
 })
 export class RegisterComponent {
 
+  private readonly authService = inject(AuthService)
+
   registerForm : FormGroup = new FormGroup({
     name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
     email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required, Validators.pattern(/^\[A-Z][a-z0-9]{6,}$/)]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Z][a-z0-9]{6,}$/)]),
     rePassword: new FormControl(null, [Validators.required]),
     phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)])
-  })
+  }, {validators : this.confirmPassword})
+
+  confirmPassword(group: AbstractControl){
+    const password = group.get('password')?.value
+    const rePassword = group.get('rePassword')?.value
+
+    if(password === rePassword){
+      console.log('Password confirmed');
+      return null
+    }
+    else{
+      console.log('Password not matched');
+      return {mismatch:true}
+    }
+  }
 
   submitForm(){
     console.log(this.registerForm);
+    console.log(this.registerForm.value);
+    this.authService.signUp(this.registerForm.value).subscribe({
+      next:(res)=>{
+        console.log(res);
+        // Navigate to Login page
+
+      },
+      error:(err)=>{
+        console.log(err);
+        // Display error
+      }
+    })
   }
 }
