7bc44ce7f6ec7a4fab2a9c00fbe74e75ad1dab7a
ShadenKhaled85 <shaden_khaled85@yahoo.com>
2025-08-05 00:11:31 +0300

    Categories component done

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute } from '@angular/router';
+import { CategoriesService } from './../../core/services/category/categories.service';
 import { Component, inject, OnInit } from '@angular/core';
+import { ICategory } from '../../shared/interfaces/icategory';
 
 @Component({
   selector: 'app-categories',
@@ -6,7 +9,30 @@ import { Component, inject, OnInit } from '@angular/core';
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+
+  private readonly categoriesService = inject(CategoriesService)
+  private readonly activatedRoute = inject(ActivatedRoute)
+
+  categories : ICategory[] = [];
+
+  catId : string = '';
+
+  ngOnInit(): void {
+    this.getCategories()
+  }
+
+  getCategories(){
+    this.categoriesService.getCategories().subscribe({
+      next: (res)=>{
+        console.log(res);
+        this.categories = res.data;
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    })
+  }
 
 
 }
