c18839f6e1345e2621c1c3807dd6b70e813e8c0a
ShadenKhaled85 <shaden_khaled85@yahoo.com>
2025-08-04 22:55:53 +0300

    Product Component done

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { CartService } from './../../core/services/cart/cart.service';
+import { IProduct } from './../../shared/interfaces/iproduct';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../core/services/product/products.service';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +9,37 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
 
+  private readonly productsService = inject(ProductsService)
+  private readonly cartService = inject(CartService)
+
+  products : IProduct[] = [];
+
+  ngOnInit(): void {
+    this.getProducts()
+  }
+
+  getProducts(){
+    this.productsService.getProducts().subscribe({
+      next:(res)=>{
+        console.log(res);
+        this.products = res.data;
+      },
+      error:(err)=>{
+        console.log(err);
+      }
+    })
+  }
+
+  addProductToCart(productId:string){
+    this.cartService.addProductToCart(productId).subscribe({
+      next:(res)=>{
+        console.log(res);
+      },
+      error:(err)=>{
+        console.log(err);
+      }
+    })
+  }
 }
